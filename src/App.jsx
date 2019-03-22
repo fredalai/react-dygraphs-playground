ee2ea82b25503a3a2889b371e760ed06d88fc073
Freda Lai <fredalai00@gmail.com>
2019-03-22 10:07:44 +0800

    Add example graph demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GraphExample from './GraphExample';
+import GridDraggable, { Section } from 'grid-draggable';
 import './App.css';
 
 import helper from './helper';
@@ -7,14 +8,76 @@ import fakeData from './fakeData';
 const labels = fakeData.labels;
 const data = fakeData.values;
 
-const App = () => (
-  <div className="App">
-    <GraphExample
-      data={data}
-      formatTimestamp={helper.formatTimestamp}
-      labels={labels}
-    />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.dragStart = this.dragStart.bind(this);
+    this.onDrag = this.onDrag.bind(this);
+    this.dragStop = this.dragStop.bind(this);
+  }
+
+  dragStart(e, data) {
+    console.log('start: ', data);
+  }
+
+  onDrag(e, data, match) {
+    console.log('drag: ', data, match);
+  }
+
+  dragStop(e, data) {
+    console.log('stop: ', data);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <GridDraggable
+          dragStart={this.dragStart}
+          onDrag={this.onDrag}
+          dragStop={this.dragStop}
+          lg={5}
+          md={3}
+          xs={6}
+          rowClassName="row-test"
+          colClassName="col-test"
+        >
+          <Section key="N1">
+            <GraphExample
+              data={data}
+              index="N1"
+              formatTimestamp={helper.formatTimestamp}
+              labels={labels}
+            />
+          </Section>
+          <Section key="N2">
+            <GraphExample
+              data={data}
+              index="N2"
+              formatTimestamp={helper.formatTimestamp}
+              labels={labels}
+            />
+          </Section>
+          <Section key="N3">
+            <GraphExample
+              data={data}
+              index="N3"
+              formatTimestamp={helper.formatTimestamp}
+              labels={labels}
+            />
+          </Section>
+          <Section key="N4">
+            <GraphExample
+              data={data}
+              index="N4"
+              formatTimestamp={helper.formatTimestamp}
+              labels={labels}
+            />
+          </Section>
+        </GridDraggable>
+      </div>
+    );
+  }
+}
 
 export default App;
